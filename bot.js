d02091d7d9f18b985191df7f5ec92bb375de6561
freshskates <business.robertcacho@gmail.com>
2021-01-03 17:05:39 -0800

    removed stuffn

--- bot.js
+++ bot.js
@@ -2,16 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fetch = require("node-fetch");
 require("dotenv").config();
-/* Create your personal token on https://lichess.org/account/oauth/token */
-const personalToken = "";
-
-fetch("https://lichess.org/api/account", {
-  headers: {
-    Authorization: "Bearer " + process.env.CHESSTOKEN,
-  },
-})
-  .then(res => res.json())
-  .then(console.log);
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
